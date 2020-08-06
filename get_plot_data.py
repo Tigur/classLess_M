c00e331b9128e6565afc2d58b27633ad2c870c76
Tigur <SirResnick2@gmail.com>
2020-08-06 14:13:40 +0200

    working plot

--- get_plot_data.py
+++ get_plot_data.py
@@ -1,4 +1,4 @@
-
+import os
 
 
 def get_data(name):
@@ -14,9 +14,55 @@ def get_data(name):
         splitted = line.split()
         if "val_loss:" in splitted:
             ind = splitted.index("val_loss:")
-            val_losses.append(splitted[ind+1])
+            val_losses.append(float(splitted[ind+1]))
 
     return val_losses
+
+
+
+def get_training_loss(name):
+    outfile_lines = []
+    losses = []
+
+    with open(name, 'rt') as outfile:
+        for line in outfile:
+            outfile_lines.append(line)
+
+    for line in outfile_lines:
+        splitted = line.split()
+        if "loss:" in splitted:
+            ind = splitted.index("loss:")
+            losses.append(float(splitted[ind+1]))
+
+    return losses
+def get_val_losses(path):
+    val_losses_list = []
+    val_losses_tuple = ()
+    for file in os.listdir(path):
+        if file[-4::]=='.log':
+            #find FL index (always 2)
+            splitted_name = file.split("_")
+            fL = int(splitted_name[2][2::])
+            #until "_" read name
+            #convert to number
+            val_losses_list.append((fL,get_data(path+file)))
+    print(f"Done making losses list at {path+file}")
+    return val_losses_list
+
+def get_losses(path):
+    losses_list = []
+    for file in os.listdir(path):
+        if file[-4::]=='.log':
+            splitted_name = file.split("_")
+            fL = int(splitted_name[2][2::])
+
+
+            losses_list.append((fL,get_training_loss(path+file)))
+    print(f"Done making losses list at {path+file}")
+    if len(losses_list) == 0:
+        print("Nothing was written")
+    return losses_list
+
 if __name__=="__main__":
     from argparse import ArgumentParser
 
@@ -29,3 +75,7 @@ if __name__=="__main__":
     name = args.filename
 
     data = get_data(name)
+
+    print(data)
+
+
--- plot_fun.py
+++ plot_fun.py
@@ -1,3 +1,60 @@
 import matplotlib
+from matplotlib import pyplot as p
+
+from get_plot_data import *
+from argparse import ArgumentParser
+import sys
+
+
+parser = ArgumentParser()
+parser.add_argument("file",
+                    help="name of the output file", type=str)
+parser.add_argument("-R", dest="isFolder",
+                    help="Flag for folder paths", action='store_true')
+args = parser.parse_args()
+
+def plot_learning_curve(file):
+    val_losses = get_data(file)
+    epochs = range(1,301,1)
+
+    p.plot(epochs,val_losses)
+    p.xlabel("epoch")
+    p.ylabel("validation loss")
+
+    p.show()
+
+def plot_best(path):
+    val_logs = get_val_losses(path)
+    train_loss_logs = get_losses(path)
+
+    if (len(val_logs) or len(train_loss_logs)) == 0:
+        print("NO DATA!! ")
+        sys.exit()
+
+
+    sv_losses = []
+    X = []
+    val_logs.sort(key=(lambda x: x[0]))
+    for fL,lossList in val_logs:
+        sv_losses.append(lossList[50])
+        X.append(fL)
+
+    p.plot(X,sv_losses)
+    p.xlabel("fragment length")
+    p.ylabel("best loss")
+
+    print(X)
+
+
+    p.show()
+
+
+if __name__=="__main__":
+    #plot_learning_curve(args.file)
+    plot_best(args.file)
+
+
+
+
 
 
