065bfec336864f3815c787599af30a257da658aa
Tigur <SirResnick2@gmail.com>
2020-08-07 03:55:35 +0200

    Working Subplots

--- plot_fun.py
+++ plot_fun.py
@@ -33,15 +33,45 @@ def plot_best(path):
 
 
     sv_losses = []
+    Xv = []
+    s_losses = []
     X = []
     val_logs.sort(key=(lambda x: x[0]))
     for fL,lossList in val_logs:
         sv_losses.append(lossList[50])
+        Xv.append(fL)
+
+
+    train_loss_logs.sort(key=(lambda x: x[0]))
+    for fL,lossList in train_loss_logs:
+        s_losses.append(lossList[50])
         X.append(fL)
 
-    p.plot(X,sv_losses)
-    p.xlabel("fragment length")
-    p.ylabel("best loss")
+
+    fig1 = p.figure(1)
+    #ax1 = fig1.add_axes([0,0,0.9,0.9])
+    ax1 = fig1.add_subplot(111)
+    l_val = ax1.plot(Xv,sv_losses,'go-')
+    fig2 = p.figure(2,linewidth=2)
+    #ax2 = fig2.add_axes([0,0,1,1])
+    ax2 = fig2.add_subplot(111)
+    l_train = ax2.plot(X,s_losses,'go-')
+    ax1.set_title("validation Losses")
+    ax2.set_title("training Losses")
+    ax1.set_xlabel("fragment Length")
+    ax2.set_xlabel("fragment Length")
+    ax1.set_ylabel("Loss")
+    ax2.set_ylabel("Loss")
+    ax1.grid(True)
+    ax2.grid(True)
+
+    ax1.spines['left'].set_linewidth(2)
+
+    ax2.autoscale(enable=True, axis='both', tight=False)
+
+    #p.plot(X,sv_losses)
+    #p.xlabel("fragment length")
+    #p.ylabel("best loss")
 
     print(X)
 
